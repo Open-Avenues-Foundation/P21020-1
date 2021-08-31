621d39a7481622c0a4a27e9b91abc60afa8e7d37
Kevin Salina <kevin.salina25@gmail.com>
2021-08-31 17:10:58 -0400

    Deleted console.logs

--- server/controllers/customers/csv-customers.js
+++ server/controllers/customers/csv-customers.js
@@ -4,16 +4,13 @@ const handleCSV = require('../../utilities/handle-csv')
 // Upload and save CSV file to database
 const upload = async (req, res) => {
   try {
-    if (req.file == undefined) {
-      return res.status(400).send("Please upload a CSV file!")
-    }
+    if (req.file == undefined) return res.status(400).send("Please upload a CSV file!")
 
     let path = __basedir + "/resources/static/assets/uploads/" + req.file.filename
 
     handleCSV(path, req, res)
 
   } catch (error) {
-    console.log('ERROR')
     console.log(error)
     res.status(500).send("Could not upload the file: " + req.file.originalname)
   }
